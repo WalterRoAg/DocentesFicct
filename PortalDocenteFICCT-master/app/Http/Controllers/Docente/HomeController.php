e2ffb3dc7ef97fce54ae9bd0cb061899e258d82d
Walter RoAg <tu_correo_de_github@correo.com>
2025-10-28 20:45:49 -0400

    Mueve proyecto a la raíz del repositorio

--- PortalDocenteFICCT-master/app/Http/Controllers/Docente/HomeController.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Http\Controllers\Docente;
-use App\Http\Controllers\Controller;
-
-class HomeController extends Controller
-{
-    public function index() { return view('docente.dashboard'); }
-}
